1b4bb759555cfde83ffeb60456044885c061eebf
MariusSilviuHriscu <hriscumarius65536@gmail.com>
2023-12-23 22:11:53 +0200

    threading support

--- ecommerce/src_base_scraper/scraper.py
+++ ecommerce/src_base_scraper/scraper.py
@@ -3,7 +3,7 @@ from typing import Protocol
 
 from .requests_handler import RequestsHandler
 from .product_info import ProductData
-
+import concurrent.futures
 
 ScraperExecutableReturnable = typing.Generator[str,None,None] | typing.Generator[ProductData,None,None]
 
@@ -26,7 +26,7 @@ class ScrapingNodeTemplate(Protocol):
     extract_data_callable : ScraperCallable
     def get_data(self) -> typing.Generator[typing.Any,None,None]:
         pass
-    def get_data_threading(self):
+    def get_data_threading(self,depth_parallelism: int):
         pass
     def set_previous_node(self):
         pass
@@ -41,7 +41,22 @@ class ScraperInitialNode :
         
         data_generator = self.extract_data_callable.get_url_data(url = self.initial_url)
         
+        
+        
         return data_generator
+    def get_data_threading(self, depth_parallelism: int) -> typing.Generator[str, None, None]:
+        if depth_parallelism > 0:
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                future_to_data = {executor.submit(self.extract_data_callable.get_url_data, url=self.initial_url): self.initial_url}
+                for future in concurrent.futures.as_completed(future_to_data):
+                    data_point = future_to_data[future]
+                    try:
+                        result = future.result()
+                        yield from result
+                    except Exception as exc:
+                        print(f"Exception occurred while processing {data_point}: {exc} init node")
+        else:
+            yield from self.get_data()
     def set_previous_node(self):
         raise ValueError('cannot set another node as previous to this')
 
@@ -57,8 +72,21 @@ class ScraperNode:
     def get_data(self) -> typing.Generator[str,None,None]:    
         previous_node_data_generator = self.previous_node.get_data()
         for data_point in previous_node_data_generator:
-            
+
             yield from self.extract_data_callable.get_url_data(url = data_point)
+    def get_data_threading(self, depth_parallelism: int) -> typing.Generator[str, None, None]:
+        if depth_parallelism > 0:
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                future_to_data = {executor.submit(self.extract_data_callable.get_url_data, url=data_point): data_point for data_point in self.previous_node.get_data_threading(depth_parallelism=depth_parallelism-1)}
+                for future in concurrent.futures.as_completed(future_to_data):
+                    data_point = future_to_data[future]
+                    try:
+                        result = future.result()
+                        yield from result
+                    except Exception as exc:
+                        print(f"Exception occurred while processing {data_point}: {exc} node")
+        else:
+            yield from self.get_data()
     def set_previous_node(self,node : ScrapingNodeTemplate):
         self.previous_node = node
 
@@ -81,7 +109,21 @@ class ProductScraperNode :
                 yield ProductData(url = data_point,
                                   product_info = product_info
                                   )
-            
+    def get_data_threading(self, depth_parallelism: int) -> typing.Generator[ProductData, None, None]:
+        if depth_parallelism > 0:
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                future_to_data = {executor.submit(self.extract_data_callable.get_url_data, url=data_point): data_point for data_point in self.previous_node.get_data_threading(depth_parallelism=depth_parallelism-1)}
+                for future in concurrent.futures.as_completed(future_to_data):
+                    data_point = future_to_data[future]
+                    try:
+                        result = future.result()
+                        product_generator = result
+                        for product_info in product_generator:
+                            yield ProductData(url=data_point, product_info=product_info)
+                    except Exception as exc:
+                        print(f"Exception occurred while processing {data_point}: {exc}")
+        else:
+            yield from self.previous_node.get_data()
     def set_previous_node(self,node : ScrapingNodeTemplate):
         self.previous_node = node
 
@@ -100,5 +142,5 @@ class ScraperManager :
     
     def get_data(self, parallel_processing_flag : bool , depth_parallelism:int=1) -> typing.Generator[ProductData, None,None] :
         if parallel_processing_flag:
-            return self.current_final_node.get
+            return self.current_final_node.get_data_threading(depth_parallelism=depth_parallelism)
         return self.current_final_node.get_data()
